b37aaf0b28e8b9edc6b660fdc11954e418f2d497
Vladimir Grichina <vgrichina@componentix.com>
2012-03-24 18:21:38 +0200

    Removed trailing whitespace

--- iGrep/CXMailDocument.m
+++ iGrep/CXMailDocument.m
@@ -22,11 +22,11 @@
             NSTextCheckingResult *match = [regex firstMatchInString:self.content options:0 range:NSMakeRange(0, self.content.length)];
             if (match && match.range.location != NSNotFound) {
                 NSRange range = [match rangeAtIndex:1];
-                
+
                 NSDateFormatter *dateFormatter = [NSDateFormatter new];
                 dateFormatter.dateFormat = @"EEE, dd MMM yyyy hh:mm:ss ZZZ";
                 dateFormatter.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US"];
-                
+
                 NSDate *parsedDate = [dateFormatter dateFromString:[self.content substringWithRange:range]];
                 if (!parsedDate) {
                     NSLog(@"Cannot parse date: %@",  [self.content substringWithRange:range]);
@@ -37,11 +37,11 @@
         } else {
             NSLog(@"Warning no content for URL: %@", self.uri);
         }
-        
+
         NSLog(@"Warning, no Date header found for URL: %@", self.uri);
         return (_date = [NSDate dateWithTimeIntervalSince1970:0]);
     }
-    
+
     return _date;
 }
 
@@ -56,17 +56,17 @@
             NSTextCheckingResult *match = [regex firstMatchInString:self.content options:0 range:NSMakeRange(0, self.content.length)];
             if (match && match.range.location != NSNotFound) {
                 NSRange range = [match rangeAtIndex:1];
-                
+
                 return (_title = [self.content substringWithRange:range]);
             }
         } else {
             NSLog(@"Warning no content for URL: %@", self.uri);
         }
-        
+
         NSLog(@"Warning, no Subject header found for URL: %@", self.uri);
         return (_title = @"< No Subject >");
     }
-    
+
     return _title;
 }
 
